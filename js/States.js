e5c4f926363b95f626c17e1a167a7ec002367e11
UMNGLAB_Dan1 <nicolasdpf@gmail.com>
2019-06-06 14:04:36 -0500

    Database

---
+++ js/States.js
@@ -0,0 +1,60 @@
+var states = [
+    //0 Ataque1
+    {
+        nombre: "ataque1",
+        probabilidades: [
+            {v: 0.70, f: 0.30}, {v: 0.40, f: 0.60},
+            {v: 0.65, f: 0.35}, {v: 0.25, f: 0.75}
+        ]
+    },
+    //1 Ataque2
+    {
+        nombre: "ataque2",
+        probabilidades: [
+            {v: 0.25, f: 0.75}, {v: 0.65, f: 0.35},
+            {v: 0.45, f: 0.55}, {v: 0.80, f: 0.20}
+        ]
+    },
+    //2 Ataque3
+    {
+        nombre: "ataque3",
+        probabilidades: [
+            {v: 0.60, f: 0.40}, {v: 0.40, f: 0.60},
+            {v: 0.90, f: 0.10}, {v: 0.25, f: 0.75}
+        ]
+    },
+    //3 Ataque4
+    {
+        nombre: "ataque4",
+        probabilidades: [
+            {v: 0, f: 0}, {v: 0, f: 0},
+            {v: 0, f: 0}, {v: 0, f: 0}
+        ]
+    },
+    //4 Defensa1
+    {
+        nombre: "Defensa1",
+        //0= J1, 1 = J2, 2 = J3, 3 = J4
+        probabilidades: [0.50, 0.15, 0.10, 0.10]
+    },
+    //5 Defensa2
+    {
+        nombre: "Defensa2",
+        //0= J1, 1 = J2, 2 = J3, 3 = J4
+        probabilidades: [0.20, 0.15, 0.10, 0.70]
+    },
+    //6 Defensa3
+    {
+        nombre: "Defensa3",
+        //0= J1, 1 = J2, 2 = J3, 3 = J4
+        probabilidades: [0.20, 0.10, 0.60, 0.10]
+    },
+    //7 Defensa4
+    {
+        nombre: "Defensa4",
+        //0= J1, 1 = J2, 2 = J3, 3 = J4
+        probabilidades: [0.10, 0.60, 0.20, 0.10]
+    }
+
+
+];
