42e358aa28d575bc30a509564a3c6d7dd1621ba6
UMNGLabAnimacion <nicolasdpf@gmail.com>
2019-06-06 13:12:50 -0500

    First steps

---
+++ js/Graph.js
@@ -0,0 +1,43 @@
+var states = [
+    //0 Ataque1
+    {
+        nombre: "ataque1"
+    },
+    //1 Ataque2
+    {
+        nombre: "ataque2"
+        
+    },
+    //2 Ataque3
+    {
+        nombre: "ataque3"
+        
+    },
+    //3 Ataque4
+    {
+        nombre: "ataque4"
+        
+    },
+    //4 Defensa1
+    {
+        nombre: "Defensa1"
+        
+    },
+    //5 Defensa2
+    {
+        nombre: "Defensa2"
+        
+    },
+    //6 Defensa3
+    {
+        nombre: "Defensa3"
+        
+    },
+    //7 Defensa4
+    {
+        nombre: "Defensa4"
+
+    }
+
+
+];
