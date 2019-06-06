9171694d81d4de0a35c5a540983aaa5c8d049bef
UMNGLAB_Dan1 <nicolasdpf@gmail.com>
2019-06-06 14:37:50 -0500

    Clase Grafo :)

---
+++ js/Graph.js
@@ -0,0 +1,33 @@
+/*jshint esversion:6*/
+class Graph{
+    constructor(){
+        this.dataList = {}
+    }
+    addVertex(vertex){
+        if(!this.dataList[vertex]){
+            this.dataList[vertex] = [];
+        }
+    }
+
+    addEdge(v1, v2){
+        this.dataList[v1].push(v2)
+        //this.dataList[v2].push(v1)
+    }
+
+    removeEdge(vertex1, vertex2)
+    {
+        this.dataList[vertex1] = this.dataList[vertex1].filter(
+            v => v !== vertex2
+        )
+        this.dataList[vertex2] = this.dataList[vertex2].filter(
+            v => v !== vertex1
+        )
+    }
+    removeVertex(vertex){
+        while(this.adjacencyList[vertex].length){
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this.adjacencyList[vertex]
+    }
+}
--- js/States.js
+++ js/States.js
@@ -3,57 +3,70 @@ var states = [
     {
         nombre: "ataque1",
         probabilidades: [
-            {v: 0.70, f: 0.30}, {v: 0.40, f: 0.60},
-            {v: 0.65, f: 0.35}, {v: 0.25, f: 0.75}
+            {v: 0.70, f: 0.30}, //Defensa 1
+            {v: 0.40, f: 0.60}, //Defensa 2
+            {v: 0.65, f: 0.35}, //Defensa 3
+            {v: 0.25, f: 0.75}  //Defensa 4
         ]
     },
     //1 Ataque2
     {
         nombre: "ataque2",
         probabilidades: [
-            {v: 0.25, f: 0.75}, {v: 0.65, f: 0.35},
-            {v: 0.45, f: 0.55}, {v: 0.80, f: 0.20}
+            {v: 0.25, f: 0.75}, //Defensa 1
+            {v: 0.65, f: 0.35}, //Defensa 2
+            {v: 0.45, f: 0.55}, //Defensa 3
+            {v: 0.80, f: 0.20}  //Defensa 4
         ]
     },
     //2 Ataque3
     {
         nombre: "ataque3",
         probabilidades: [
-            {v: 0.60, f: 0.40}, {v: 0.40, f: 0.60},
-            {v: 0.90, f: 0.10}, {v: 0.25, f: 0.75}
+            {v: 0.60, f: 0.40}, //Defensa 1
+            {v: 0.40, f: 0.60}, //Defensa 2
+            {v: 0.90, f: 0.10}, //Defensa 3
+            {v: 0.25, f: 0.75}  //Defensa 4
         ]
     },
     //3 Ataque4
     {
         nombre: "ataque4",
         probabilidades: [
-            {v: 0, f: 0}, {v: 0, f: 0},
-            {v: 0, f: 0}, {v: 0, f: 0}
+            {v: 0.49, f: 0.51}, //Defensa 1
+            {v: 0.80, f: 0.20}, //Defensa 2
+            {v: 0.60, f: 0.40}, //Defensa 3
+            {v: 0.45, f: 0.55}  //Defensa 4
         ]
     },
     //4 Defensa1
     {
         nombre: "Defensa1",
         //0= J1, 1 = J2, 2 = J3, 3 = J4
-        probabilidades: [0.50, 0.15, 0.10, 0.10]
+        probabilidades: [0.50, 0.15, 0.10, 0.10],
+        ataques: [{a: 0}, {a: 1}, {a: 2}, {a: 3}]
     },
     //5 Defensa2
     {
         nombre: "Defensa2",
         //0= J1, 1 = J2, 2 = J3, 3 = J4
-        probabilidades: [0.20, 0.15, 0.10, 0.70]
+        probabilidades: [0.20, 0.15, 0.10, 0.70],
+        ataques: [{a: 0}, {a: 1}, {a: 2}, {a: 3}]
     },
     //6 Defensa3
     {
         nombre: "Defensa3",
         //0= J1, 1 = J2, 2 = J3, 3 = J4
-        probabilidades: [0.20, 0.10, 0.60, 0.10]
+        probabilidades: [0.20, 0.10, 0.60, 0.10],
+        ataques: [{a: 0}, {a: 1}, {a: 2}, {a: 3}]
     },
     //7 Defensa4
     {
         nombre: "Defensa4",
         //0= J1, 1 = J2, 2 = J3, 3 = J4
-        probabilidades: [0.10, 0.60, 0.20, 0.10]
+
+        probabilidades: [0.10, 0.60, 0.20, 0.10],
+        ataques: [{a: 0}, {a: 1}, {a: 2}, {a: 3}]
     }
 
 
