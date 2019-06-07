fae15e280c45205c71a8097d9b8c6080e21f4ae1
UMNGLAB_Dan1 <nicolasdpf@gmail.com>
2019-06-07 12:23:36 -0500

    Listo el calculo de Probabilidad Condicional

--- js/States.js
+++ js/States.js
@@ -1,74 +1,3 @@
-var states = [
-    //0 Ataque1
-    {
-        nombre: "ataque1",
-        probabilidades: [
-            {v: 0.70, f: 0.30}, //Defensa 1
-            {v: 0.40, f: 0.60}, //Defensa 2
-            {v: 0.65, f: 0.35}, //Defensa 3
-            {v: 0.25, f: 0.75}  //Defensa 4
-        ]
-    },
-    //1 Ataque2
-    {
-        nombre: "ataque2",
-        probabilidades: [
-            {v: 0.25, f: 0.75}, //Defensa 1
-            {v: 0.65, f: 0.35}, //Defensa 2
-            {v: 0.45, f: 0.55}, //Defensa 3
-            {v: 0.80, f: 0.20}  //Defensa 4
-        ]
-    },
-    //2 Ataque3
-    {
-        nombre: "ataque3",
-        probabilidades: [
-            {v: 0.60, f: 0.40}, //Defensa 1
-            {v: 0.40, f: 0.60}, //Defensa 2
-            {v: 0.90, f: 0.10}, //Defensa 3
-            {v: 0.25, f: 0.75}  //Defensa 4
-        ]
-    },
-    //3 Ataque4
-    {
-        nombre: "ataque4",
-        probabilidades: [
-            {v: 0.49, f: 0.51}, //Defensa 1
-            {v: 0.80, f: 0.20}, //Defensa 2
-            {v: 0.60, f: 0.40}, //Defensa 3
-            {v: 0.45, f: 0.55}  //Defensa 4
-        ]
-    },
-    //4 Defensa1
-    {
-        nombre: "Defensa1",
-        //0= J1, 1 = J2, 2 = J3, 3 = J4
-        probabilidades: [0.50, 0.15, 0.10, 0.10],
-        ataques: [{a: 0}, {a: 1}, {a: 2}, {a: 3}]
-    },
-    //5 Defensa2
-    {
-        nombre: "Defensa2",
-        //0= J1, 1 = J2, 2 = J3, 3 = J4
-        probabilidades: [0.20, 0.15, 0.10, 0.70],
-        ataques: [{a: 0}, {a: 1}, {a: 2}, {a: 3}]
-    },
-    //6 Defensa3
-    {
-        nombre: "Defensa3",
-        //0= J1, 1 = J2, 2 = J3, 3 = J4
-        probabilidades: [0.20, 0.10, 0.60, 0.10],
-        ataques: [{a: 0}, {a: 1}, {a: 2}, {a: 3}]
-    },
-    //7 Defensa4
-    {
-        nombre: "Defensa4",
-        //0= J1, 1 = J2, 2 = J3, 3 = J4
-        probabilidades: [0.10, 0.60, 0.20, 0.10],
-        ataques: [{a: 0}, {a: 1}, {a: 2}, {a: 3}]
-    }
-];
-
 var ataques = [
     {
         nombre: "ataque1",
@@ -111,28 +40,28 @@ var ataques = [
 var defensas = [
     //0 Defensa1
     {
-        nombre: "Defensa1",
+        nombre: "defensa1",
         //0= A1, 1 = A2, 2 = A3, 3 = A4
         probabilidades: [0.50, 0.15, 0.10, 0.10],
         ataques: [{a: 0}, {a: 1}, {a: 2}, {a: 3}]
     },
     //1 Defensa2
     {
-        nombre: "Defensa2",
+        nombre: "defensa2",
         //0= J1, 1 = J2, 2 = J3, 3 = J4
         probabilidades: [0.20, 0.15, 0.10, 0.70],
         ataques: [{a: 0}, {a: 1}, {a: 2}, {a: 3}]
     },
     //2 Defensa3
     {
-        nombre: "Defensa3",
+        nombre: "defensa3",
         //0= J1, 1 = J2, 2 = J3, 3 = J4
         probabilidades: [0.20, 0.10, 0.60, 0.10],
         ataques: [{a: 0}, {a: 1}, {a: 2}, {a: 3}]
     },
     //3 Defensa4
     {
-        nombre: "Defensa4",
+        nombre: "defensa4",
         //0= J1, 1 = J2, 2 = J3, 3 = J4
         probabilidades: [0.10, 0.60, 0.20, 0.10],
         ataques: [{a: 0}, {a: 1}, {a: 2}, {a: 3}]
--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 console.log("Hola Mundo");
 
-
 let grafoBY = new Graph();
 
 for (let i = 0; i < ataques.length; i++) {
@@ -13,11 +12,7 @@ for (let i = 0; i < defensas.length; i++) {
     grafoBY.addVertex(defensas[i].nombre);
 }
 
-
-
-console.log(grafoBY);
-
-function findIndex(ataque){
+function findIndexAtaque(ataque){
     let temp;
     for (let i = 0; i < ataques.length; i++) {
         if(ataque === ataques[i].nombre){
@@ -27,12 +22,55 @@ function findIndex(ataque){
     }
     return temp;
 }
-
-function calculateProbabilities(ataque){
-    let iTarget = findIndex(ataque);
-    let target = ataques[iTarget];
+function findIndexDefensa(defensa){
+    let temp;
+    for (let i = 0; i < defensas.length; i++) {
+        if(defensa === defensas[i].nombre){
+            temp = i;
+            continue;
+        }        
+    }
+    return temp;
+}
+/**
+ * 
+ * @param {Graph vertex} ataqueX 
+ * @param {Graph vertex} defensaTarget 
+ */
+function calcularProbCondicinal(ataqueX, defensaTarget){
+    let iTarget = findIndexAtaque(ataqueX); ///Ayuda a ubicar el index dentro de la estructura de datos
+    let probAtaqueX = ataques[iTarget];//Guarda las probabilidades del ataque X segun cada defensa
+    let probDefensaN = [] //Guarda el conjunto de probabilidad total de las defensas en una jugada específica
+    let iDTarget = findIndexDefensa(defensaTarget); //Guarda la probabilidad de la defensa target dada la jugada de ataque X
+    let deftarget; 
+    let pDnA; // Guarda la variable resultante de hallar P(D n A)
+    let pA = 0;
     
-    console.log(target)
+    //1. Obtener los datos probabilisticos del ataque X segun cada defensa N
+    probAtaqueX = probAtaqueX.probabilidades;
+    console.log(probAtaqueX);
+    //2. Obtener las probabilidades de seleccionar una defensa en una jugada de ataque X
+    defensas.forEach(defensa => {
+        probDefensaN.push(defensa.probabilidades[iTarget]);
+    });
+    
+    //3. Definir Probabilidad de defensa target dado un ataque X
+    deftarget = defensas[iDTarget].probabilidades[iTarget];
+
+    //4. hallar P( D n A)
+    pDnA = deftarget * probAtaqueX[iTarget].v;
+
+    //5. Hallar P(D n A)
+    console.log(probDefensaN)
+    for (let i = 0; i < probDefensaN.length; i++) {
+        var temp = probDefensaN[i] * probAtaqueX[i].v;
+        pA += temp;
+    }
+
+    
+    let resultado = (pDnA / pA) * 100;
+    console.log(resultado.toFixed(2))
+    return resultado.toFixed(2);
 }
 
-calculateProbabilities("ataque1")
+calcularProbCondicinal("ataque3", "defensa4");
