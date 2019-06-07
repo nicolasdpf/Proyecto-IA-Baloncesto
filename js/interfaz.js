5d295d4f9fa79f63475b37b217db0fc6ef3c730a
UMNGLAB_Dan1 <nicolasdpf@gmail.com>
2019-06-07 15:56:37 -0500

    Jejejejejjejejeje I'm Iron Man

--- js/interfaz.js
+++ js/interfaz.js
@@ -1,17 +1,60 @@
+/**
+ * Declaracion de variables iniciales 
+ */
+
+
 let botonInicial = document.getElementById("btnIniciar");
 
+let btnAtaque1 = document.getElementById("ataque1");
+let btnAtaque2 = document.getElementById("ataque2");
+let btnAtaque3 = document.getElementById("ataque3");
+let btnAtaque4 = document.getElementById("ataque4");
+
+let seleccionAtaque = document.getElementById("ataqueSeleccionado");
 
 
 
 
+/**
+ * Declaracion de Event Listeners
+ */
+btnAtaque1.addEventListener("click", ejecutarAtq1);
+btnAtaque2.addEventListener("click", ejecutarAtq2);
+btnAtaque3.addEventListener("click", ejecutarAtq3);
+btnAtaque4.addEventListener("click", ejecutarAtq4);
 
-botonInicial.addEventListener("click", restAPI);
 
 
+/**
+ * Funciones
+ */
+function ejecutarAtq1(){
+    madeProcess("ataque1");
+}
+
+function ejecutarAtq2(){
+    madeProcess("ataque2");
+}
+
+function ejecutarAtq3(){
+    madeProcess("ataque3");
+}
+
+function ejecutarAtq4(){
+    madeProcess("ataque4");
+}
 
 
-function restAPI(){
+function madeProcess(ataque){
+
+    let def1 = calcularProbCondicinal(ataque, "defensa1");
+    let def2 = calcularProbCondicinal(ataque, "defensa2");
+    let def3 = calcularProbCondicinal(ataque, "defensa3");
+    let def4 = calcularProbCondicinal(ataque, "defensa4");
+
     
-
-
+    console.log(def1);
+    console.log(def2);
+    console.log(def3);
+    console.log(def4);
 }
